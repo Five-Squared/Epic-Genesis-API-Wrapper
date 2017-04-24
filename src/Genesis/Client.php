5050e3eab75055846d43686e9134b84c1528109d
Christian Burger <christianjburger@gmail.com>
2017-04-24 10:48:45 +0200

    Added support for PUT requests

--- src/Genesis/Client.php
+++ src/Genesis/Client.php
@@ -22,8 +22,59 @@ class Genesis_Client
 
         return $this->get($this->_endpoint . '/' . $path);
     }
+
+    public function putByPath($path, $data)
+    {
+        if (!$this->_endpoint) {
+            throw new \Exception('Method not available when endpoint is not configured');
+        }
+
+        return $this->put($this->_endpoint . '/' . $path, $data);
+    }
     
     public function get($link)
+    {
+        $uri = $this->_getUri($link);
+        
+        $response = $this->_getClient($uri)->restGet($uri->getPath());
+
+        if (!$response) {
+            throw new Exception('Error fetching from ' . $href);
+        }
+
+        return $this->_getResource($response);
+    }
+
+    public function put($link, $data)
+    {
+        $uri = $this->_getUri($link);
+
+        $response = $this->_getClient($uri)->restPut($uri->getPath(), $data);
+
+        if (!$response) {
+            throw new Exception('Error sending request to ' . $href);
+        }
+        
+        return $this->_getResource($response);
+    }
+
+    protected function _getResource($response)
+    {
+        $xml = simplexml_load_string($response->getBody());
+        if (!$xml) {
+            throw new Exception('Failed to load XML from ' . $href);
+        }
+        
+        $resource = Genesis_Factory::createResource($xml);
+
+        if ($resource instanceof Genesis_Resource_Error) {
+            throw new Exception('Error response returned (' . $response->getStatus() . ') with message ' . $response->getBody());
+        }
+    
+        return $resource;
+    }
+
+    protected function _getUri($link)
     {
         if ($link instanceof SimpleXMLElement && $link->getName() == 'Link') {
             $href = (string) $link['href'];
@@ -34,32 +85,22 @@ class Genesis_Client
         if (empty($href)) {
             throw new Exception('No href for link ' . $link);
         }
-        $uri = Zend_Uri_Http::fromString($href);
+
+        return Zend_Uri_Http::fromString($href);
+    }
+
+    protected function _getClient($uri)
+    {
+        $url = $uri->getScheme() . '://' . $uri->getHost();
         
-        $client = new Zend_Rest_Client($uri->getScheme() . '://' . $uri->getHost());
+        $client = new Zend_Rest_Client($url);
+
         $client->getHttpClient()
             ->resetParameters(true)
             ->setHeaders('Accept','application/xml')
             ->setAuth($this->_username, $this->_password, Zend_Http_Client::AUTH_BASIC)
             ->setConfig(array('timeout' => 60));
-        
-        $response = $client->restGet($uri->getPath());
-        if (!$response) {
-            throw new Exception('Error fetching from ' . $href);
-        }
-        
-        $xml = simplexml_load_string($response->getBody());
-        if (!$xml) {
-            throw new Exception('Failed to load XML from ' . $href);
-        }
-        
-        $resource = Genesis_Factory::createResource($xml);
 
-        if ($resource instanceof Genesis_Resource_Error) {
-            //todo get decent error messages from API
-            throw new Exception('Error object returned when calling ' . $href);
-        }
-    
-        return $resource;
+        return $client;
     }
 }
